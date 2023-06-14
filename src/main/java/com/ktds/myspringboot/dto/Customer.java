f6568eb84ce9f8582ad07ecf05063b409837d7e6
myvega2k <vega2k@kakao.com>
2023-06-14 15:03:36 +0900

    customer add

---
+++ src/main/java/com/ktds/myspringboot/dto/Customer.java
@@ -0,0 +1,15 @@
+package com.ktds.myspringboot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Customer {
+    private String name;
+    private int age;
+}
