ff49ee399ef5cb39099205308588b9cd856c1b04
myvega2k <vega2k@kakao.com>
2023-06-14 13:41:58 +0900

    envionment variable add

--- src/main/java/com/ktds/myspringboot/runner/MyRunner.java
+++ src/main/java/com/ktds/myspringboot/runner/MyRunner.java
@@ -1,13 +1,17 @@
 package com.ktds.myspringboot.runner;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements ApplicationRunner {
+    @Value("${myboot.name}")
+    private String name;
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println("환경변수 name 값 = " + name);
         System.out.println("Program Argument = " + args.containsOption("foo"));
         System.out.println("VM Argument = " + args.containsOption("bar"));
     }
