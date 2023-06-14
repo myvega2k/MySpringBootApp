1247372e158bd22e9bbe810fb701e83d6ae515f3
myvega2k <vega2k@kakao.com>
2023-06-14 14:19:41 +0900

    Properties class add

--- src/main/java/com/ktds/myspringboot/runner/MyRunner.java
+++ src/main/java/com/ktds/myspringboot/runner/MyRunner.java
@@ -1,5 +1,7 @@
 package com.ktds.myspringboot.runner;
 
+import com.ktds.myspringboot.property.MyBootProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -12,9 +14,15 @@ public class MyRunner implements ApplicationRunner {
     @Value("${myboot.age}")
     private int age;
 
+    @Autowired
+    private MyBootProperties properties;
+
     //java -jar target\MySpringBoot-App-0.0.1-SNAPSHOT.jar --myboot.name=Spring
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Properties getFullName 값 = " + properties.getFullName());
+        System.out.println("Properties getAGe 값 = " + properties.getAge());
+
         System.out.println("환경변수 name 값 = " + name);
         System.out.println("환경변수 age 값 = " + age);
         System.out.println("Program Argument = " + args.containsOption("foo"));
