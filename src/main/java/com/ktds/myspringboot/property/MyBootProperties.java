cd1ce0175e390d7f838f57136a7670b57c29025b
myvega2k <vega2k@kakao.com>
2023-06-14 14:17:28 +0900

    Properties class add

---
+++ src/main/java/com/ktds/myspringboot/property/MyBootProperties.java
@@ -0,0 +1,15 @@
+package com.ktds.myspringboot.property;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("myboot")
+@Getter @Setter
+public class MyBootProperties {
+    private String name;
+    private int age;
+    private String fullName;
+}
