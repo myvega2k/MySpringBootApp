20b4b51c724cab752b94b19600c08b419805b604
myvega2k <vega2k@kakao.com>
2023-06-15 16:53:53 +0900

    modelmapper add

--- src/main/java/com/ktds/myspringboot/service/UserEntityService.java
+++ src/main/java/com/ktds/myspringboot/service/UserEntityService.java
@@ -4,18 +4,22 @@ import com.ktds.myspringboot.dto.UserRequest;
 import com.ktds.myspringboot.dto.UserResponse;
 import com.ktds.myspringboot.entity.UserEntity;
 import com.ktds.myspringboot.repository.UserEntityRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class UserEntityService {
     private final UserEntityRepository repository;
-
+    private final ModelMapper modelMapper;
     //constructor injection
-    public UserEntityService(UserEntityRepository repository) {
-        this.repository = repository;
-    }
+//    public UserEntityService(UserEntityRepository repository, ModelMapper modelMapper) {
+//        this.repository = repository;
+//        this.modelMapper = modelMapper;
+//    }
 
     public UserResponse insertUser(UserRequest userRequest) {
         UserEntity userEntity = UserEntity.builder()
