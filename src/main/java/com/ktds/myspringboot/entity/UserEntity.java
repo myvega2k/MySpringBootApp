9224cdb420658804c2701d86484f322990f8a970
myvega2k <vega2k@kakao.com>
2023-06-15 16:18:02 +0900

    user entity add

--- src/main/java/com/ktds/myspringboot/entity/UserEntity.java
+++ src/main/java/com/ktds/myspringboot/entity/UserEntity.java
@@ -1,12 +1,14 @@
 package com.ktds.myspringboot.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
@@ -15,5 +17,18 @@ import org.hibernate.annotations.DynamicUpdate;
 @Setter
 @DynamicUpdate
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false, updatable = false, insertable = false)
+    @ColumnDefault(value = "CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
 
 }
