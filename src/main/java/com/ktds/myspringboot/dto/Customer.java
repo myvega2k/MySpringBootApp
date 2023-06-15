52880b8a42c90142ca554d8df8137548fd6e4269
myvega2k <vega2k@kakao.com>
2023-06-15 13:47:25 +0900

    lambda

--- src/main/java/com/ktds/myspringboot/dto/Customer.java
+++ src/main/java/com/ktds/myspringboot/dto/Customer.java
@@ -1,14 +1,12 @@
 package com.ktds.myspringboot.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Customer {
     private String name;
     private int age;
--- src/test/java/com/ktds/myspringboot/LambdaTest.java
+++ src/test/java/com/ktds/myspringboot/LambdaTest.java
@@ -1,8 +1,16 @@
 package com.ktds.myspringboot;
 
+import com.ktds.myspringboot.dto.Customer;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class LambdaTest {
+
+    @Test
+    void consumer_test() {
+        List.of(new Customer("boot",10), new Customer("msa",20));
+    }
 
     @Test
     void runnable_test () {
@@ -15,7 +23,8 @@ public class LambdaTest {
         });
         t1.start();
         //2. Lambda Expression
-
+        Thread t2 = new Thread(() -> System.out.println("람다함수 형태로 재정의함"));
+        t2.start();
 
 
     }
