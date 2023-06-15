60263971d18c56ab7edfcb818e4edc49053c2f04
myvega2k <vega2k@kakao.com>
2023-06-15 11:05:05 +0900

    Repository add

--- src/test/java/com/ktds/myspringboot/repository/AccountRepositoryTest.java
+++ src/test/java/com/ktds/myspringboot/repository/AccountRepositoryTest.java
@@ -4,15 +4,19 @@ import com.ktds.myspringboot.entity.Account;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class AccountRepositoryTest {
     @Autowired
     AccountRepository accountRepository;
 
     @Test
+    @Rollback(value = false)
     void insert_select() {
         //Account 객체생성
         Account account =
