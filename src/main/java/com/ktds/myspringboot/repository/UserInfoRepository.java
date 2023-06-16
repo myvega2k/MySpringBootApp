16b0ea3d5ec75c29c137bbf93044b71ef63cdbef
myvega2k <vega2k@kakao.com>
2023-06-16 16:48:53 +0900

    security config add

--- src/main/java/com/ktds/myspringboot/repository/UserInfoRepository.java
+++ src/main/java/com/ktds/myspringboot/repository/UserInfoRepository.java
@@ -3,5 +3,9 @@ package com.ktds.myspringboot.repository;
 import com.ktds.myspringboot.entity.UserInfo;
 import org.springframework.data.repository.ListCrudRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface UserInfoRepository extends ListCrudRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByEmail(String username);
 }
