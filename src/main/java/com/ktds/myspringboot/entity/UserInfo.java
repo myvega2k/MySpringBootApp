b334e123f5e0390d8aa48ec12f0530bbc31f6228
myvega2k <vega2k@kakao.com>
2023-06-16 16:39:56 +0900

    security config add

--- src/main/java/com/ktds/myspringboot/entity/UserInfo.java
+++ src/main/java/com/ktds/myspringboot/entity/UserInfo.java
@@ -1,9 +1,6 @@
 package com.ktds.myspringboot.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,7 +14,10 @@ public class UserInfo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+    @Column(unique = true, nullable = false)
     private String email;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
     private String roles;
 }
