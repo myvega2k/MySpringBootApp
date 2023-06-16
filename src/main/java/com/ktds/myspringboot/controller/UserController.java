76ccb318399e0b5820e48717cd5efed17624bae3
myvega2k <vega2k@kakao.com>
2023-06-16 14:57:46 +0900

    usercontroller update

--- src/main/java/com/ktds/myspringboot/controller/UserController.java
+++ src/main/java/com/ktds/myspringboot/controller/UserController.java
@@ -1,11 +1,23 @@
 package com.ktds.myspringboot.controller;
 
+import com.ktds.myspringboot.service.UserEntityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final UserEntityService service;
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("users",service.listUser());
+        return "index";
+    }
+
     @GetMapping("/thymeleaf")
     public String leaf(Model model) {
         model.addAttribute("name","스프링부트");
