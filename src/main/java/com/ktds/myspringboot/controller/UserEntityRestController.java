fcdd6f80f8854d2e93feb4d93be3e2eb3fab5e5d
myvega2k <vega2k@kakao.com>
2023-06-16 10:50:46 +0900

    controller update

--- src/main/java/com/ktds/myspringboot/controller/UserEntityRestController.java
+++ src/main/java/com/ktds/myspringboot/controller/UserEntityRestController.java
@@ -5,10 +5,9 @@ import com.ktds.myspringboot.dto.UserResponse;
 import com.ktds.myspringboot.service.UserEntityService;
 import jakarta.persistence.Entity;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +18,10 @@ public class UserEntityRestController {
     @PostMapping
     public UserResponse addUser(@RequestBody UserRequest request) {
         return service.insertUser(request);
+    }
+
+    @GetMapping
+    public List<UserResponse> listUser() {
+        return service.listUser();
     }
 }
